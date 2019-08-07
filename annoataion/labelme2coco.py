599e6486e99ef785269f3642d35e7c09eddb6f9f
peter850421 <peter850421@yahoo.com.tw>
2019-08-07 11:10:48 +0800

    Correct the wrong format

--- annoataion/labelme2coco.py
+++ annoataion/labelme2coco.py
@@ -10,6 +10,7 @@ from labelme import utils
 import numpy as np
 import glob
 import PIL.Image
+import os
 
 class MyEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -24,10 +25,6 @@ class MyEncoder(json.JSONEncoder):
 
 class labelme2coco(object):
     def __init__(self, labelme_json=[], save_json_path='./tran.json'):
-        '''
-        :param labelme_json: 所有labelme的json文件路径组成的列表
-        :param save_json_path: json保存位置
-        '''
         self.labelme_json = labelme_json
         self.save_json_path = save_json_path
         self.images = []
@@ -38,6 +35,12 @@ class labelme2coco(object):
         self.annID = 1
         self.height = 0
         self.width = 0
+
+        #Keep train and valid having same label
+        self.label_name = ['cone_o','traffic_sign_o','cone_x','rod_x','rod_o','traffic_sign_x','traffic_sign_night']
+        for i in self.label_name:
+            self.categories.append(self.categorie(i))
+            self.label.append(i)
 
         self.save_json()
 
@@ -52,17 +55,13 @@ class labelme2coco(object):
                     if label not in self.label:
                         self.categories.append(self.categorie(label))
                         self.label.append(label)
-                    points = shapes['points']#这里的point是用rectangle标注得到的，只有两个点，需要转成四个点
-                    points.append([points[0][0],points[1][1]])
-                    points.append([points[1][0],points[0][1]])
+                    points = shapes['points']
                     self.annotations.append(self.annotation(points, label, num))
                     self.annID += 1
 
     def image(self, data, num):
         image = {}
-        img = utils.img_b64_to_arr(data['imageData'])  # 解析原图片数据
-        # img=io.imread(data['imagePath']) # 通过图片路径打开图片
-        # img = cv2.imread(data['imagePath'], 0)
+        img = utils.img_b64_to_arr(data['imageData'])
         height, width = img.shape[:2]
         img = None
         image['height'] = height
@@ -78,7 +77,7 @@ class labelme2coco(object):
     def categorie(self, label):
         categorie = {}
         categorie['supercategory'] = 'component'
-        categorie['id'] = len(self.label) + 1  # 0 默认为背景
+        categorie['id'] = len(self.label) + 1  # 0 background
         categorie['name'] = label
         return categorie
 
@@ -87,12 +86,9 @@ class labelme2coco(object):
         annotation['segmentation'] = [list(np.asarray(points).flatten())]
         annotation['iscrowd'] = 0
         annotation['image_id'] = num + 1
-        # annotation['bbox'] = str(self.getbbox(points)) # 使用list保存json文件时报错（不知道为什么）
-        # list(map(int,a[1:-1].split(','))) a=annotation['bbox'] 使用该方式转成list
         annotation['bbox'] = list(map(float, self.getbbox(points)))
         annotation['area'] = annotation['bbox'][2] * annotation['bbox'][3]
-        # annotation['category_id'] = self.getcatid(label)
-        annotation['category_id'] = 1
+        annotation['category_id'] = self.getcatid(label)
         annotation['id'] = self.annID
         return annotation
 
@@ -100,22 +96,14 @@ class labelme2coco(object):
         for categorie in self.categories:
             if label == categorie['name']:
                 return categorie['id']
-        return 1
+        return -1
 
     def getbbox(self, points):
-        # img = np.zeros([self.height,self.width],np.uint8)
-        # cv2.polylines(img, [np.asarray(points)], True, 1, lineType=cv2.LINE_AA)  # 画边界线
-        # cv2.fillPoly(img, [np.asarray(points)], 1)  # 画多边形 内部像素值为1
         polygons = points
-
         mask = self.polygons_to_mask([self.height, self.width], polygons)
         return self.mask2box(mask)
 
     def mask2box(self, mask):
-        '''从mask反算出其边框
-        mask：[h,w]  0、1组成的图片
-        1对应对象，只需计算1对应的行列号（左上角行列号，右下角行列号，就可以算出其边框）
-        '''
         # np.where(mask==1)
         index = np.argwhere(mask == 1)
         rows = index[:, 0]
@@ -132,7 +120,7 @@ class labelme2coco(object):
         # return [(left_top_c, left_top_r), (right_bottom_c, right_bottom_r)]
         # return [left_top_c, left_top_r, right_bottom_c, right_bottom_r]  # [x1,y1,x2,y2]
         return [left_top_c, left_top_r, right_bottom_c - left_top_c,
-                right_bottom_r - left_top_r]  # [x1,y1,w,h] 对应COCO的bbox格式
+                right_bottom_r - left_top_r]  # [x1,y1,w,h] COCO bbox format
 
     def polygons_to_mask(self, img_shape, polygons):
         mask = np.zeros(img_shape, dtype=np.uint8)
@@ -153,10 +141,8 @@ class labelme2coco(object):
         self.data_transfer()
         self.data_coco = self.data2coco()
         # 保存json文件
-        json.dump(self.data_coco, open(self.save_json_path, 'w'), indent=4, cls=MyEncoder)  # indent=4 更加美观显示
+        json.dump(self.data_coco, open(self.save_json_path, 'w'), indent=4, cls=MyEncoder)
 
 
-labelme_json = glob.glob('./*.json')
-# labelme_json=['./1.json']
-
-labelme2coco(labelme_json, './new.json')
+labelme_json = sorted(glob.glob('./*.json')) # search the json to transform to coco
+labelme2coco(labelme_json, 'valid.json') #output valid.json
